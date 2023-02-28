5215b8a0f915e0037ac3e904f2dc67e26cbbd763
Danilo P <danilo.pena@endava.com>
2023-02-28 18:12:47 -0500

    Textures part 2

--- Chapter_4/Mesh3D.py
+++ Chapter_4/Mesh3D.py
@@ -1,4 +1,5 @@
 from OpenGL.GL import *
+import pygame
 
 
 class Mesh3D:
@@ -11,6 +12,9 @@ class Mesh3D:
                          (-0.5, 0.5, -0.5)
                          ]
         self.triangles = [0, 2, 3, 0, 3, 1]
+        self.draw_type = GL_LINE_LOOP
+        self.texture = pygame.image.load()
+        self.texID = 0
 
     def draw(self):
         for t in range(0, len(self.triangles), 3):
@@ -22,3 +26,15 @@ class Mesh3D:
             glVertex3fv(self.vertices[self.triangles[t +
                                                      2]])
             glEnd()
+
+    def init_texture(self):
+        self.texID = glGenTextures(1)
+        texture_data = pygame.image.tostring(self.texture, "RGB", False)
+        width = self.texture.get_width()
+        height = self.texture.get_height()
+        glBindTexture(GL_TEXTURE_2D, self.texID)
+        glTexParameteri(GL_TEXTURE_2D,
+                        GL_TEXTURE_MIN_FILTER,
+                        GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0,
+                     GL_RGB, GL_UNSIGNED_BYTE, texture_data)
---
+++ Chapter_5/Textures.py
@@ -0,0 +1,45 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from Cube import *
+from OpenGL.GLU import *
+
+pygame.init()
+screen_width = 1000
+screen_height = 800
+screen = pygame.display.set_mode((screen_width, screen_height), DOUBLEBUF | OPENGL)
+pygame.display.set_caption('OpenGL in Python, Lights Practice')
+
+done = False
+white = pygame.Color(255, 255, 255)
+glMatrixMode(GL_PROJECTION)
+camera_aspect_ratio = screen_width / screen_height
+gluPerspective(60, camera_aspect_ratio, 0.1, 100)
+glMatrixMode(GL_MODELVIEW)
+glTranslatef(0, 0, -3)
+
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_LIGHTING)
+
+glLight(GL_LIGHT0, GL_POSITION, (5, 5, 5, 1))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (1, 0, 0, 1))
+glEnable(GL_LIGHT0)
+glMaterialfv(GL_FRONT, GL_DIFFUSE, (1, 1, 0, 1))
+
+print(GL_MAX_LIGHTS)
+cube = Cube(GL_POLYGON, '../Materials/Plastic_SpaceBlanketFolds_512_albedo.tiff')
+rotate_cube = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            rotate_cube = not rotate_cube
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    cube.draw()
+    if rotate_cube:
+        glRotatef(0.01, 3, 0.5, 0)
+    pygame.display.flip()
+
+pygame.quit()
